67ac78c245fc709b6f78e2df064c1d07943644b1
Andrea Bernardini <andrebask@gmail.com>
2014-05-02 11:34:10 +0200

    lexer restored (I overwrited it by mistake). return type is optional (see grammar)

--- lexer.py
+++ lexer.py
@@ -2,36 +2,81 @@
 
 __doc__='''Simple lexer for PL/0 using generators'''
 
+## # Tokens can have multiple definitions if needed
+## symbols =  {
+## 	'lparen' : ['('],
+## 	'rparen' : [')'],
+## 	'times'  : ['*'],
+## 	'slash'  : ['/'],
+## 	'plus'   : ['+'],
+## 	'minus'  : ['-'],
+## 	'eql'    : ['='],
+## 	'neq'    : ['!='],
+## 	'lss'    : ['<'],
+## 	'leq'    : ['<='],
+## 	'gtr'    : ['>'],
+## 	'geq'    : ['>='],
+## 	'callsym': ['call'],
+## 	'beginsym'  : ['begin'],
+## 	'semicolon' : [';'],
+## 	'endsym'    : ['end'],
+## 	'ifsym'     : ['if'],
+## 	'whilesym'  : ['while'],
+## 	'becomes'   : [':='],
+## 	'thensym'   : ['then'],
+## 	'dosym'     : ['do'],
+## 	'constsym'  : ['const'],
+## 	'comma'     : [','],
+## 	'varsym'    : ['var'],
+## 	'procsym'   : ['procedure'],
+## 	'period'    : ['.'],
+## 	'oddsym'    : ['odd'],
+## 	'print'			: ['!', 'print'],
+## }
+
 # Tokens can have multiple definitions if needed
 symbols =  {
-	'lparen' : ['('],
-	'rparen' : [')'],
-	'times'  : ['*'],
-	'slash'  : ['/'],
-	'plus'   : ['+'],
-	'minus'  : ['-'],
-	'eql'    : ['='],
-	'neq'    : ['!='],
-	'lss'    : ['<'],
-	'leq'    : ['<='],
-	'gtr'    : ['>'],
-	'geq'    : ['>='],
-	'callsym': ['call'],
-	'beginsym'  : ['begin'],
-	'semicolon' : [';'],
-	'endsym'    : ['end'],
-	'ifsym'     : ['if'],
-	'whilesym'  : ['while'],
-	'becomes'   : [':='],
-	'thensym'   : ['then'],
-	'dosym'     : ['do'],
-	'constsym'  : ['const'],
-	'comma'     : [','],
-	'varsym'    : ['var'],
-	'procsym'   : ['procedure'],
-	'period'    : ['.'],
-	'oddsym'    : ['odd'],
-	'print'			: ['!', 'print'],
+	'lparen'        : ['('],
+	'rparen'        : [')'],
+	'times'         : ['*'],
+	'slash'         : ['/'],
+	'plus'          : ['+'],
+	'minus'         : ['-'],
+    'mod_sym'       : ['mod'],
+	'eql'           : ['eq'],
+	'neq_sym'       : ['neq'],
+	'lss'           : ['<'],
+	'leq'           : ['<='],
+	'gtr'           : ['>'],
+	'geq'           : ['>='],
+    'and_sym'       : ['and'],
+    'or_sym'        : ['or'],
+    'not_sym'       : ['not'],
+	'if_sym'        : ['if'],
+    'else_sym'      : ['_sym'],
+	'becomes'       : ['<-'],
+	'comma'         : [','],
+	'period'        : ['.'],
+	'at_sym'        : ['@'],
+	'hash_sym'      : ['#'],
+    'int_sym'       : ['int'],
+    'float_sym'     : ['float'],
+    'bool_sym'      : ['bool'],
+    'char_sym'      : ['char'],
+    'vect_sym'      : ['vector'],
+    'of_sym'        : ['of'],
+    'quote_sym'     : ['\''],
+    'incr_sym'      : ['incr'],
+    'decr_sym'      : ['decr'],
+    'ret_sym'       : ['return'],
+    'repeat_sym'    : ['repeat'],
+    'until_sym'     : ['until'],
+    'for_sym'       : ['foreach'],
+    'in_sym'        : ['in'],
+    'dec_sym'       : ['dec'],
+    'colon'         : [':'],
+    'block_sym'     : ['->'],
+    'is_sym'        : ['is']
 }
 
 def token(word):
@@ -57,22 +102,19 @@ def lexer(text) :
 
 
 # Test support
-__test_program='''VAR x, squ;
+__test_program='''
 
-PROCEDURE square;
-BEGIN
-   squ := x * x
-END;
+dec square int x: int ->
+    return x * x.
+.
 
-BEGIN
-   x := 1;
-   WHILE x <= 10 DO
-   BEGIN
-      CALL square;
-      x := -x;
-			!squ
-   END
-END.'''
+dec main ->
+    square 2.
+.
+
+main.
+
+'''
 
 if __name__ == '__main__' :
 	for t,w in lexer(__test_program) :
